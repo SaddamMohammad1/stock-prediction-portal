850e7cb9c0601ec5dc3cfa007354f46e064c6351
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-04-26 21:15:15 +0530

    Install and setup rest_framework_simplejwt for token, access token and refresh token, and Implement login functionality

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
     'accounts',
     'api',
 ]
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
